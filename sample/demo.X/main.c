1bde2dc9d425deba7f2e14747d935966373c17eb
jwalkerbg <i_cenov@botevgrad.com>
2019-02-05 23:08:45 +0200

    RT #3519: Removed redundant comments.

--- sample/demo.X/main.c
+++ sample/demo.X/main.c
@@ -1,8 +1,5 @@
 /* main.h
  *
- * This file is part of the XXX distribution (https://github.com/xxxx or http://xxx.github.io).
- * Copyright (c) 2015 Liviu Ionescu.
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
